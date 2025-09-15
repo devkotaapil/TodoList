dd2a90a38755921a9673aa078398be3d999cb72a
Request <devkotaapil194@gmail.com>
2025-09-15 17:19:24 +0545

    initial stage

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+import TodoList from  './components/TodoList'
+import AddTodo from './components/AddTodo';
+
+function App(){
+  const [todos,setTodos] = useState([]);
+
+
+  const addTodo = (text) =>{
+    const newTodo = {id:Date.now(), text, completed:false};
+    setTodos([...todos, newTodo]);
+  }
+
+  const toggleTodo = (id) =>{
+      setTodos(todos.map(todo=>
+        todo.id === id ? {...todo, completed: !todo.completed} : todo
+      ));
+  }
+  const deleteTodo = (id) =>{
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+ 
+  return(
+    <div className='min-h-screen bg-gray-100 py-8'>
+      <div className='max-w-md mx-auto bg-white rounded-lg shadow-md p-6'>
+        <h1 className='text-2xl font-bold text-center mb-6 text-gray-800'>My Todo List</h1>
+        <AddTodo onaddTodo={addTodo}/>
+        <TodoList todos={todos} onToggleTodo={toggleTodo} onDeleteTodo={deleteTodo}/>
+      </div>
+    </div>
+
+    
+  )
+}
+
+export default App;
---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {useState} from 'react';
+
+function AddTodo({onAddTodo}){
+  const [input, setInput] = useState('');
+  
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(input.trim()){
+      onAddTodo(input);
+      setInput('');
+    }
+  };
+
+  return(
+    <form onSubmit={handleSubmit} className='mb-6' >
+      <div className='flex'>
+        <input type="text" value={input} onChange={(e)=> setInput(e.target.value)}
+        placeholder='Add new todo...' 
+        className='bg-gray-300 flex-1 p-2 rounded-1-md focus:outline-none focus:ring-2 focus:ring-blue-500 ' />
+        <button
+        type='sumbit' 
+        className='px-4 py-2 bg-blue-500 text-white rounded-r-md hover:bg-blue-600'
+        >Add</button>
+      </div>
+    </form>
+  )
+
+}
+
+export default AddTodo
