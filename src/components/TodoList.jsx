7ade39e68748c5c8f6e4ac50a40eb26aaa00dc01
Request <devkotaapil194@gmail.com>
2025-09-15 17:41:00 +0545

    adding TodoList component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import TodoItem from './TodoItem';
 
-const TodoList = () => {
-  return (
-    <div>TodoList</div>
+function TodoList({todos, onToggleTodo, onDeleteTodo}){
+  if(todos.length === 0){
+    return <p className='text-center text-gray-500 mt-4'>No todos yet. Add one!</p>
+  }
+
+  return(
+    <ul className='space-y-2'>
+      {todos.map(todo =>(
+        <TodoItem 
+        key={todo.id}
+        todo={todo}
+        onToggleTodo={onToggleTodo}
+        onDeleteTodo={onDeleteTodo}
+        />
+      ))}
+    </ul>
   )
 }
 
-export default TodoList
+export default TodoList;
